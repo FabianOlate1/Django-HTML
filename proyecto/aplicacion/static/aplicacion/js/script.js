f277928860170c8b6c3214244b7715d714eaea20
Fabian Olate <101122038+FabianOlate1@users.noreply.github.com>
2022-06-29 01:18:10 -0400

    yes

--- proyecto/aplicacion/static/aplicacion/js/script.js
+++
@@ -1,9 +0,0 @@
-const mapdiv = document.getElementById("map");
-let map
-function initmap() {
-  map = new google.maps.Map(mapdiv,{
-    center: {lat:-34.5956145 ,lng: -58.4431949},
-    zoom : 8
-  })
-}
-
